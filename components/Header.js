1c49a7ebf1f863d088fd86404ad1c04cc8acf9a8
Alok <alok.dubey22@spit.ac.in>
2023-04-08 18:49:37 +0530

    add menu and profile image at right side of header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
         
-      <div className='flex items-center justify-between max-w-6xl' >
+      <div className='flex items-center justify-between max-w-6xl mx-4  xl:mx-auto' >
         {/* {left} */}
 
             <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
@@ -32,9 +33,13 @@ export default function Header() {
                 <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md'/>
             </div>
 
-        {/* {Right} */}
+        {/* {Right} */} 
 
-            <h1> Right side</h1>
+        <div className="flex space-x-4 items-center">
+            <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+            <PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+            <img src='alok_image.jpg' alt='user-image' className='h-10 rounded-full cursor-pointer'/>
+        </div>
 
         </div>
 
