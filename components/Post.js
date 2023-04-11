14b6712f82030819f9ba593f90a5aa0d08624ee1
Alok <alok.dubey22@spit.ac.in>
2023-04-12 01:06:48 +0530

    update recent work add caption...

--- components/Post.js
+++ components/Post.js
@@ -28,10 +28,10 @@ export default function Post({img,userImg,caption,username,id}) {
 
         {/* {Post comments} */}
 
-        <p className='p-5 truncate'><span className='font-bold mr-3'>{username}</span>{caption}</p>
+        <p className='p-5 truncate'><span className='font-bold mr-2'>{username}</span>{caption}</p>
 
         {/* {Post input box} */}
-        <form className='flex item-center p-4'>
+        <form className='flex items-center p-4'>
             <EmojiHappyIcon className='h-7'/>
             <input className='border-none flex-1 focus:ring-0' type ="text" placeholder="Enter your comment..."/>
             <button className='text-blue-500 font-bold'>Post</button>
