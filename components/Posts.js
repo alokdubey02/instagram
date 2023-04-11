b56f36a8d26f11ddc116fda4f0f6f023c2770874
Alok <alok.dubey22@spit.ac.in>
2023-04-12 00:33:07 +0530

    dummy post image changes through unsplash

--- components/Posts.js
+++ components/Posts.js
@@ -7,20 +7,20 @@ export default function Posts() {
             id:"1",
             username:"Kumar",
             userImg:"alok_image.jpg",
-            img:"https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            img:"https://source.unsplash.com/900x900/?nature,girl",
             caption: "Thanks for "
         } ,
          {
             id:"2",
             username:"Alok",
             userImg:"alok_image.jpg",
-            img:"https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            img:"https://source.unsplash.com/900x900/?girl,fitness",
             caption: "Nice picture "
         },  {
             id:"3",
             username:"Dubey",
             userImg:"alok_image.jpg",
-            img:"https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            img:"https://source.unsplash.com/900x900/?girl,water",
             caption: "Thanks for "
         }
     ]
