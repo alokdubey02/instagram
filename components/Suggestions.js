9c356d2479ed0486e44625bf741a410e8de30190
Alok <alok.dubey22@spit.ac.in>
2023-05-21 02:11:34 +0530

    solve looping in suggestion feed

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import minifaker from 'minifaker';
+import "minifaker/locales/en";
 
 export default function Suggestions() {
-  const [suggestions, setSuggestions] = useState([])
+  const [suggestions, setSuggestions] = useState([]);
   useEffect (() => {
     const suggestions = minifaker.array(5,(i)=>(
     {
         username: minifaker.username({locale: "en"}).toLowerCase(),
         jobTitle: minifaker.jobTitle(),
-        id: i
+        id: i,
     }
     ))
     setSuggestions(suggestions);
-  })
+  },[])
   return (
     <div className='mt-4 ml-10'>
       <div className="flex justify-between mb-5 text-sm">
