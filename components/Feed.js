b8adb1ce6622bcf06f656b686e44fb9b7c1a4cf3
Alok <alok.dubey22@spit.ac.in>
2023-05-21 02:03:14 +0530

    buildSuggestion partOf theFeed compnt(feed loop)

--- components/Feed.js
+++ components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
+import Suggestions from './Suggestions'
 
 export default function Feed() {
   return (
@@ -25,6 +26,7 @@ export default function Feed() {
                 <MiniProfile/>
 
             {/* {suggestion} */}
+                <Suggestions/>
             </div>
         </section>
         
