4ec1f2bc8542ccb70fc31472d9e4d0ba95c37f27
Alok <alok.dubey22@spit.ac.in>
2023-05-22 01:10:47 +0530

    hide button & input section when user is loggedOut

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,12 @@ import { HomeIcon } from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atom/modalAtom';
+import { useRouter } from 'next/router';
 
 export default function Header() {
     const {data: session} = useSession();
     const [open, setOpen] = useRecoilState(modalState);
+    const router = useRouter();
     
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
@@ -21,6 +23,7 @@ export default function Header() {
                 <Image  src={"/instagram_logo.png"}
                 layout="fill"
                 className='object-contain'
+                onClick={()=>router.push("/")}
                 />
 
             </div>
@@ -28,6 +31,7 @@ export default function Header() {
                 <Image  src={"/instagram_logo_small.png"}
                 layout="fill"
                 className='object-contain'
+                onClick={()=>router.push("/")}
                 />
 
             </div>
@@ -44,7 +48,7 @@ export default function Header() {
         {/* {Right} */} 
 
         <div className="flex space-x-4 items-center">
-            <HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+            <HomeIcon onClick={()=>router.push("/")} className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
             {session ?(
                 <>
                     <PlusCircleIcon onClick={()=> setOpen(true)} className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
