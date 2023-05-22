8f7120e2fca5086d5dc367abc85c2ad4dd965649
Alok <alok.dubey22@spit.ac.in>
2023-05-22 21:38:34 +0530

    update signin.js paragraph

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -12,7 +12,7 @@ export default function signin({ providers}) {
                 {Object.values(providers).map((provider) =>(
                   <div key={provider.name} className='flex flex-col items-center'>
                     <img className='w-32 object-cover' src='https://techstory.in/wp-content/uploads/2021/12/instagram-logo-2-2.png' alt=''/>
-                    <p className='text-sm italic my-10 text-center'> This app is created for learning purpose</p>
+                    <p className='text-sm italic my-10 text-center'> This app is created for learning purpose By Alok Dubey</p>
                     <button onClick={()=> signIn(provider.id, {callbackUrl: "/"})} className='bg-red-400 rounded-lg p-3 text-white hover:bg-red-500 '>Sign in with {provider.name}</button>
                   </div>
                 ))}
